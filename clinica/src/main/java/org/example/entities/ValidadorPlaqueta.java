b4a7e9312407a2e8288311eed51c2ce9334f5fc8
Fernando Julio <julio8martins@gmail.com>
2025-08-23 16:35:30 -0300

    fix: Chain of Responsability - ExameTipo parametro

--- clinica/src/main/java/org/example/entities/ValidadorPlaqueta.java
+++ clinica/src/main/java/org/example/entities/ValidadorPlaqueta.java
@@ -1,13 +1,13 @@
 package org.example.entities;
 
 import java.util.List;
-
+import org.example.entities.abstracts.ExameTipo;
 import org.example.entities.abstracts.ValidadorLaboratorial;
 
 public class ValidadorPlaqueta extends ValidadorLaboratorial{
     
     @Override
-    public boolean validar(List<String> dados) {
+    public boolean validar(List<String> dados, ExameTipo exame) {
         System.out.println("Validando Plaqueta");
         if (dados.isEmpty()) {
             return false;
--- clinica/src/main/java/org/example/entities/interfaces/IValidador.java
+++ clinica/src/main/java/org/example/entities/interfaces/IValidador.java
@@ -1,8 +1,10 @@
 package org.example.entities.interfaces;
 
+import org.example.entities.models.Exame;
+import org.example.entities.abstracts.ExameTipo;
 import java.util.List;
 
 public interface IValidador {
-    public boolean validar(List<String> dados);
+    public boolean validar(List<String> dados, ExameTipo exame);
     public IValidador setProximo(IValidador validador);
 }
