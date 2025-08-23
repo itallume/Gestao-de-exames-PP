6b422ddb7400301fccbf716f1b1bec1dd8b44860
Fernando Julio <julio8martins@gmail.com>
2025-08-23 15:57:53 -0300

    fix: Chain of Responsability - validadores

--- clinica/src/main/java/org/example/entities/abstracts/ValidadorImagem.java
+++ clinica/src/main/java/org/example/entities/abstracts/ValidadorImagem.java
@@ -3,9 +3,11 @@ package org.example.entities.abstracts;
 import org.example.entities.interfaces.IValidador;
 
 public abstract class ValidadorImagem implements IValidador {
-    
+    private IValidador proximo;
+
     @Override
     public IValidador setProximo(IValidador proximo) {
+        this.proximo = proximo;
         return proximo;
     }
 }
