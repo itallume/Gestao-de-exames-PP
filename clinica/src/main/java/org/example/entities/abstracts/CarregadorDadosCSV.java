63fc337d113bd753be48ca5e7f18bb6dd14e549b
Fernando Julio <julio8martins@gmail.com>
2025-08-24 17:44:17 -0300

    feat: logs

--- clinica/src/main/java/org/example/entities/abstracts/CarregadorDadosCSV.java
+++ clinica/src/main/java/org/example/entities/abstracts/CarregadorDadosCSV.java
@@ -3,21 +3,28 @@ package org.example.entities.abstracts;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
 
 public abstract class CarregadorDadosCSV {
-    
-    public List<String> carregarDados(String resourcePath){
+    private static final Logger logger = LoggerFactory.getLogger(CarregadorDadosCSV.class);
+
+    public List<String> carregarDados(String resourcePath) {
         List<String> dados = new ArrayList<String>();
         System.out.println("Carregando dados de: " + resourcePath);
+        logger.info("Carregando dados de: " + resourcePath);
         return dados;
     }
 
-    public void fecharArquivo(File arq){
+    public void fecharArquivo(File arq) {
         System.out.println("Fechando arquivo: " + arq.getPath());
     }
 
     public abstract List<Record> parseDados(List<String> dados);
+
     public abstract List<Record> processarDados(List<Record> records);
+
     public abstract List<Record> salvarDados(List<Record> records);
-    
+
 }
--- clinica/src/main/java/org/example/entities/models/Pagamento.java
+++ clinica/src/main/java/org/example/entities/models/Pagamento.java
@@ -5,29 +5,34 @@ import org.example.entities.interfaces.ExameVisitor;
 
 import java.util.Date;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Pagamento implements Desconto {
     private List<Exame> exames;
     private Date dataPagamento;
     private Paciente paciente;
+    private static final Logger logger = LoggerFactory.getLogger(Pagamento.class);
 
-    public Pagamento(List<Exame> exames, Paciente paciente){
+    public Pagamento(List<Exame> exames, Paciente paciente) {
         this.exames = exames;
         this.paciente = paciente;
     }
 
-    public Double calcularPreco(ExameVisitor<Double> visitor){
+    public Double calcularPreco(ExameVisitor<Double> visitor) {
         double preco = 0;
-        for (Exame e : exames){
+        for (Exame e : exames) {
             preco += e.calcularPreco(visitor);
         }
         return preco;
     }
 
     public void pagar() {
-        for (Exame e : exames){
-            System.out.println("Pagando exame de Id: "+ e.getId());
+        for (Exame e : exames) {
+            logger.info("paciente: " + paciente.getNome() + " - Tentando Pagar exame de Id: " + e.getId());
+            System.out.println("Pagando exame de Id: " + e.getId());
             e.setPago(true);
+            logger.info("paciente: " + paciente.getNome() + " - Pagou exame de Id: " + e.getId());
         }
 
         this.dataPagamento = new Date();
